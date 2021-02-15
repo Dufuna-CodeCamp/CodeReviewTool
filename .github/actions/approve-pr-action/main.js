9bca118fd55f34d2824a6cada1fc82d7266b4527
Folashade Daniel <beccadaniels1824@gmail.com>
2021-02-15 19:22:25 +0100

    update approval action

---
+++ .github/actions/approve-pr-action/main.js
@@ -0,0 +1,41 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { GitHub } = require("@actions/github/lib/utils");
+
+async function run() {
+    try {
+        const status = core.getInput("status");
+        const token = core.getInput("github-token");
+
+        const pr = github.context.payload;
+
+        if (!pr) {
+            throw new Error("Event payload is missing `pull_request`");
+        }
+
+        const client = new GitHub(token)
+        core.debug(`Checking review for pull request #${pr.number}`);
+
+        if (status == 'PASS') {
+            await client.pulls.createReview({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                pull_number: pr.number,
+                event: "APPROVE"
+            });
+            core.debug(`Approved pull request #${pr.number}`);
+        } else {
+            await client.pulls.createReview({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                pull_number: pr.number,
+                event: "REQUEST_CHANGES"
+            });
+        }
+
+    } catch(error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
