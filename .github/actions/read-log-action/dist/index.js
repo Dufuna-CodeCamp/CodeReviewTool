8a65d1f96571016078de624d6366fcb6523e7cd5
Folashade Daniel <beccadaniels1824@gmail.com>
2021-02-15 16:10:38 +0100

    update ncc build

--- .github/actions/read-log-action/dist/index.js
+++ .github/actions/read-log-action/dist/index.js
@@ -14,8 +14,7 @@ function getContent() {
         const filePath = core.getInput('path-to-log-file');
 
         fs.readFile(filePath, 'utf8', (error, data) => {
-            console.log(data)
-            core.setOutput("log-file-content", content)
+            core.setOutput("log-file-content", data)
         });
     
     } catch(error) {
