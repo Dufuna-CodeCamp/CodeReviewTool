7aa548558e50a651833df9db84ac1aee71758687
Folashade Daniel <beccadaniels1824@gmail.com>
2021-02-17 12:48:28 +0100

    allow disable merge after test passes

--- .github/actions/approve-pr-action/dist/index.js
+++ .github/actions/approve-pr-action/dist/index.js
@@ -29,20 +29,13 @@ async function run() {
         core.debug(`Checking review for pull request #${pr.number}`);
 
         if (status == 'PASS') {
-            // await octokitClient.pulls.createReview({
-            //     owner: github.context.repo.owner,
-            //     repo: github.context.repo.repo,
-            //     pull_number: pr.number,
-            //     event: "APPROVE"
-            // });
-            core.debug(`Approved pull request #${pr.number}`);
-
-            await octokitClient.pulls.merge({
+            await octokitClient.pulls.createReview({
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo,
                 pull_number: pr.number,
-                merge_method: "merge"
-            })
+                event: "APPROVE"
+            });
+            core.debug(`Approved pull request #${pr.number}`);
         } else {
             await octokitClient.pulls.createReview({
                 owner: github.context.repo.owner,
