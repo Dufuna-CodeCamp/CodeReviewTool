392b8613377b371e61a10b4c5f1a106ed2e829a0
Folashade Daniel <beccadaniels1824@gmail.com>
2021-03-15 15:50:57 +0100

    a test

--- .github/actions/read-log-action/main.js
+++ .github/actions/read-log-action/main.js
@@ -3,7 +3,6 @@ const github = require("@actions/github");
 const fs = require('fs');
 
 async function checkLogExistenceInPR({ owner, repo, pull_number, path }) {
-    core.setFailed("Just a test");
     try {
         token = core.getInput("repo-token");
         const octokitClient = github.getOctokit(token);
@@ -35,19 +34,19 @@ async function checkLogExistenceInPR({ owner, repo, pull_number, path }) {
     }
 }
 
-function getContent() {
+// function getContent() {
 
-    try {
-        const filePath = core.getInput('path-to-log-file');
+//     try {
+//         const filePath = core.getInput('path-to-log-file');
 
-        fs.readFile(filePath, 'utf8', (error, data) => {
-            core.setOutput("log-file-content", data)
-        });
+//         fs.readFile(filePath, 'utf8', (error, data) => {
+//             core.setOutput("log-file-content", data)
+//         });
     
-    } catch(error) {
-        core.setFailed(error.message);
-    }
-}
+//     } catch(error) {
+//         core.setFailed(error.message);
+//     }
+// }
 
 checkLogExistenceInPR({ 
     owner: github.context.repo.owner, 
@@ -55,7 +54,7 @@ checkLogExistenceInPR({
     path: core.getInput('path-to-log-file')
 }).then( doesExist => {
     if (doesExist) {
-        getContent()
+        // getContent()
     } else {
         throw new Error("Build failed because no log file is present, changes are requested!");
     }
--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,6 @@ const github = __nccwpck_require__(177);
 const fs = __nccwpck_require__(747);
 
 async function checkLogExistenceInPR({ owner, repo, pull_number, path }) {
-    core.setFailed("Just a test");
     try {
         token = core.getInput("repo-token");
         const octokitClient = github.getOctokit(token);
@@ -42,19 +41,19 @@ async function checkLogExistenceInPR({ owner, repo, pull_number, path }) {
     }
 }
 
-function getContent() {
+// function getContent() {
 
-    try {
-        const filePath = core.getInput('path-to-log-file');
+//     try {
+//         const filePath = core.getInput('path-to-log-file');
 
-        fs.readFile(filePath, 'utf8', (error, data) => {
-            core.setOutput("log-file-content", data)
-        });
+//         fs.readFile(filePath, 'utf8', (error, data) => {
+//             core.setOutput("log-file-content", data)
+//         });
     
-    } catch(error) {
-        core.setFailed(error.message);
-    }
-}
+//     } catch(error) {
+//         core.setFailed(error.message);
+//     }
+// }
 
 checkLogExistenceInPR({ 
     owner: github.context.repo.owner, 
@@ -62,7 +61,7 @@ checkLogExistenceInPR({
     path: core.getInput('path-to-log-file')
 }).then( doesExist => {
     if (doesExist) {
-        getContent()
+        // getContent()
     } else {
         throw new Error("Build failed because no log file is present, changes are requested!");
     }
